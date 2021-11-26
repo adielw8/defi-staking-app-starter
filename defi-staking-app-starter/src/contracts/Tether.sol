40331f40d8a69115306939baf6de5cf5bd732c2b
adielw8 <adielw8@gmail.com>
2021-11-26 12:31:26 +0200

    template contract

---
+++ defi-staking-app-starter/src/contracts/Tether.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+
+contract Tether {
+    string public name = "Test Tether Token";
+    string public symble = "TUSDT";
+    uint256 public totalSupply = 1000000000000000000;
+    uint8 public decimals = 18;
+
+}
