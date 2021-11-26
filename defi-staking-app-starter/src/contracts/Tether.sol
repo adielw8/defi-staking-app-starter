3c37b433a2b6fac082c433ec2df0c4a14dbe4b1a
adielw8 <adielw8@gmail.com>
2021-11-26 17:06:07 +0200

    token

--- defi-staking-app-starter/src/contracts/Tether.sol
+++ defi-staking-app-starter/src/contracts/Tether.sol
@@ -7,4 +7,50 @@ contract Tether {
     uint256 public totalSupply = 1000000000000000000;
     uint8 public decimals = 18;
 
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint _value
+    );
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    constructor(){
+        balanceOf[msg.sender] = totalSupply;
+    } 
+
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transfer(address _to,uint _value) public returns (bool success){
+        address _from = msg.sender;
+        require(balanceOf[_from] >= _value);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[msg.sender][_from] -=_value; 
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+        require(balanceOf[_from] >= _value);
+        require(allowance[msg.sender][_from] >= _value);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+
+    }
+
+
+
 }
